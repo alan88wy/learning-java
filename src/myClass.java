b90f9a8b3c07cdc206d69547bcaf1a648b57abd2
alan88wy <10415010+alan88wy@users.noreply.github.com>
2022-10-29 22:39:14 +0800

    Modify myclass

--- src/myClass.java
+++ src/myClass.java
@@ -3,28 +3,56 @@ import java.util.Objects;
 
 public class myClass {
     public static void main(String[] args) {
-        Employee a = new Employee(123, "a", 1000.0, "MI6", LocalDate.now());
+        Employee a = new Employee("a", 1000.0, "MI6", LocalDate.now());
+        Employee b = new Employee("b", 1000.0, "MI6", LocalDate.now());
+
+        System.out.println("A : " + a.toString());
+        System.out.println("B : " + b.toString());
+        System.out.println("A == B ? " + a.equals(b));
+        System.out.print("A hascode == b hash code ? " );
+        System.out.println(a.hashCode() == b.hashCode());
+        System.out.print("a.toString == b.toString ? ");
+        System.out.println(a.toString() == b.toString());
+
+        System.out.println("Setting a's name to James Bond ");
 
         a.setName("James Bond");
-        
+
         System.out.println(a.toString());
 
+        Employee c = new Employee();
+
+        c.setName("Thor");
+        c.setDepartment("X-Team");
+        c.setSalary(0.00);
+        System.out.println("C : " + c.toString());
     }
 }
 
 class Employee {
+    private static int nextId = 0;
     private int id;
     private String name;
     private double salary;
     private String department;
     private LocalDate startDate;
 
-    public Employee(int id, String name, double salary, String department, LocalDate startDate) {
-        this.id = id;
+    public Employee() {
+        this.id = this.getNextId();
+        this.salary = 0.00;
+        this.startDate = LocalDate.now();
+    }
+
+    public Employee(String name, double salary, String department, LocalDate startDate) {
+        this.id = this.getNextId();
         this.name = name;
         this.salary = salary;
         this.department = department;
         this.startDate = startDate;
+    }
+
+    public int getNextId() {
+        return ++this.nextId;
     }
 
     public int getId() {
