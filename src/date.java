53533b6a78bc916ba0acc51b677892d925534f11
alan88wy <10415010+alan88wy@users.noreply.github.com>
2022-10-29 10:27:13 +0800

    Added Date

---
+++ src/date.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.Date
+
+public class date {
+    public static void main(String[] args) {
+        LocalDate a = LocalDate.now().plusMonths(2);
+
+        System.out.println(a);
+
+        System.out.println("Year : " + a.getYear());
+        System.out.println("Month : " + a.getMonth());
+        System.out.println("Month (Int) " + a.getMonthValue());
+        System.out.println("Day : " + a.getDayOfMonth());
+
+
+
+    }
+}
