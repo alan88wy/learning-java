9b0945e1c93b34e8c6af2cf35f777120cdd1635a
alan88wy <alan88w@yahoo.com>
2022-11-08 21:09:09 +0800

    added lambda.java

---
+++ src/lambda.java
@@ -0,0 +1,17 @@
+
+public class lambda {
+    public static void main(String [] args) {
+        Prt p = new Prt() {
+            public void draw() {
+                for (int i = 1; i <=10; i++) System.out.println(i);
+            };
+        };
+
+        p.draw();
+        
+    }
+}
+
+interface Prt {
+    public void draw();
+}
