4ee730d49f150ad9b1ba82cca2069a7cf1485942
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-06-11 17:54:50 +0800

    Removed empty spaces

--- src/arithmetic.java
+++ src/arithmetic.java
@@ -9,9 +9,9 @@ class BasicsDemo {
         System.out.print(" ");                // Print a space
         System.out.print("world!!");
     }
-	
+
 	static void primitives() {
-        System.out.println("\n\nInside primitives ..."); 
+        System.out.println("\n\nInside primitives ...");
         // literals demo
         int intHex = 0x0041;
         System.out.println("intHex: " + intHex);
@@ -21,7 +21,7 @@ class BasicsDemo {
         System.out.println("intChar: " + intChar);
         int intUnderscore = 1_23_456;
         System.out.println("intUnderscore: " + intUnderscore);
-        
+
         char charA = 'A';
         System.out.println("charA: " + charA);
         char charInt = 65;
@@ -40,45 +40,45 @@ class BasicsDemo {
         long y = 42;
         //int x = y;
         int x = (int)y;
-        
+
         // Information loss due to out-of-range assignment
         byte narrowdByte = (byte)123456;
-        System.out.println("narrowdByte: " + narrowdByte); 
-        
+        System.out.println("narrowdByte: " + narrowdByte);
+
         // Truncation
         int iTruncated = (int)0.99;
-        System.out.println("iTruncated: " + iTruncated); 
-        
+        System.out.println("iTruncated: " + iTruncated);
+
         // Implicit cast (int to long)
         y = x;
-        
+
         // Implicit cast (char to int)
         char cChar = 'A';
         int iInt = cChar; 
         System.out.println("iInt: " + iInt);
-        
+
         // byte to char using an explicit cast
         byte bByte = 65;
         cChar = (char)bByte; // special conversion (widening from byte --> int followed by narrowing from int --> char)
         System.out.println("cChar: " + cChar);
     }
-	
+
 	static void arrays() {
         System.out.println("\nInside arrays ...");
         int[] myArray = new int[]{9, 11, 2, 5, 4, 4, 6};
         System.out.println("myArray.length: " + myArray.length);
         System.out.println("myArray[1]: " + myArray[1]);
-        System.out.println("myArray[7]: " + myArray[7]);	   
+        System.out.println("myArray[7]: " + myArray[7]);
 	}
 
     static void threeDimensionalArrays() {
         System.out.println("\nInside threeDimensionalArrays ...");
-        int[][][] unitsSold = new int[][][]{ 
+        int[][][] unitsSold = new int[][][]{
                 { // New York
                     {0,0,0,0}, // Jan
                     {0,0,0,0}, // Feb
                     {0,0,0,0}, // Mar
-                    {0,850,0,0}// Apr	
+                    {0,850,0,0}// Apr
                 },
                 { // San Francisco
                     {0,0,0,0}, // Jan
@@ -86,7 +86,7 @@ class BasicsDemo {
                     {0,0,0,0}, // Mar
                     {0,0,0,0}  // Apr
                 },
-                { 
+                {
                     {0,0,0,0},
                     {0,0,0,0},
                     {0,0,0,0},
@@ -99,48 +99,47 @@ class BasicsDemo {
                     {0,0,0,0}
                 }
         };
-                                    
+
         System.out.println("unitsSold[0][3][1]: " + unitsSold[0][3][1]);
-	}	
-	
+	}
+
 	static void varargsOverload(boolean b, int i, int j, int k){
         System.out.println("\nInside varargsOverload without varargs ...");
 	}
 	static void varargsOverload(boolean b, int... list){
         System.out.println("\nInside varargsOverload with varargs ...");
         System.out.println("list.length: " + list.length);
-	}	
-	
+	}
+
 	static void preAndPost() {
 		System.out.println("\nInside preAndPost ...");
-		
-		// int x = 5;
-		
-		//--x;		
-		//System.out.println("x: " + x);	
 
-		//int y = x--;		
+		// int x = 5;
+
+		//--x;
+		//System.out.println("x: " + x);
+
+		//int y = x--;
 		//System.out.println("y: " + y + ", x: " +  x);
-		
+
 		int index = 0;
 		int[] array = new int[3];
-		array[index++] = 10;		
-		array[index++] = 20;		
+		array[index++] = 10;
+		array[index++] = 20;
 		array[index++] = 30;
-		
+
 		System.out.println(index);
 	}
-	
-	
+
 	static void compoundArithmeticAssignment() {
 		int x = 100;
-		
+
 		System.out.println("x += 5: " + (x += 5));
 		System.out.println("x -= 5: " + (x -= 5));
 		System.out.println("x *= 5: " + (x *= 5));
 		System.out.println("x /= 5: " + (x /= 5));
 		System.out.println("x %= 5: " + (x %= 5));
-		
+
 		// Invalid
 		System.out.println("x =+ 5: " + (x =+ 5)); // Unary plus ~ x = +5
 		System.out.println("x =- 5: " + (x =- 5)); // Unary minus ~ x = -5
@@ -148,34 +147,34 @@ class BasicsDemo {
 		System.out.println("x =/ 5: " + (x =/ 5));
 		System.out.println("x =% 5: " + (x =% 5));*/
 	}
-	
+
 	static void isOddOrEven(int num) {
 		System.out.println(num % 2);
 	}
-	
+
 	static void charTypePromotion() {
         System.out.println("\nInside charTypePromotion ...");
         char char1 = 50; // Will be assigned corresponding UTF16 value 2
         System.out.println("char1: " + char1); 
         System.out.println("(73 - char1): " + (73 - char1)); // char1 gets promoted to int, i.e., decimal equivalent 50 in UTF16 is used	  
-        System.out.println("(char1 - '3'): " + (char1 - '3')); // char1 & '3' are promoted to ints	
+        System.out.println("(char1 - '3'): " + (char1 - '3')); // char1 & '3' are promoted to ints
         System.out.println("('a' + 'b'): " + ('a' +'b')); // 'a' & 'b' are promoted to ints and the respective equivalents 97 & 98 are added
     }
-		
-	public static void main(String[] args) {	
+
+	public static void main(String[] args) {
         // Language Basics 1
-        //print();				
+        //print();
         //primitives();
         //typeCasting();
-        //arrays();	
-        //threeDimensionalArrays();	
+        //arrays();
+        //threeDimensionalArrays();
         /*varargsOverload(true, 1, 2, 3);
-        varargsOverload(true, 1, 2, 3, 4, 5, 6, 7, 8);	  
+        varargsOverload(true, 1, 2, 3, 4, 5, 6, 7, 8);
         varargsOverload(true);*/
-        
+
         //preAndPost();
         //compoundArithmeticAssignment();
         //isOddOrEven(51);
         charTypePromotion();
-    }       
-  }
+    }
+}
