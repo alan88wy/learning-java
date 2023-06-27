df682b39b540a694df512c1b6c77839064cfb819
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-06-27 21:53:31 +0800

    Changes Integer division

--- src/Bitwise.java
+++ src/Bitwise.java
@@ -19,11 +19,30 @@ public class Bitwise {
                             j + " (" + Integer.toBinaryString(j) + ") = " +
                             (i ^ j) + " (" + Integer.toBinaryString(i ^ j) + ")");
         
-        System.out.println(" ~ " + i + " (~" + Integer.toBinaryString(i) + ") = " + (~i) +
+        System.out.println("~" + i + " (~" + Integer.toBinaryString(i) + ") = " + (~i) +
                             " (" + Integer.toBinaryString(~i) + ")");
         
-        
-        
+        // Shift Operator
+        j = i << 1;
+        System.out.println("Bitwise Shift Operator");
+        System.out.println(i + " << 1 (" + Integer.toBinaryString(i) + " << 1) = " + j +
+                            " (" + Integer.toBinaryString(j) + ")");
+        i = 13;
+        j = i >> 1;
+        System.out.println(i + " >> 1 (" + Integer.toBinaryString(i) + " >> 1) = " + j +
+                            " (" + Integer.toBinaryString(j) + ")");
+
+        System.out.println("Unsigned Right Shift. Similar to divide by 2");
+        i = 13;
+        j = i >>> 1;
+        double k = i / 2.0;  // I think better to do k = (double) i / 2.0;
+        System.out.println(i + " >>> 1 (" + Integer.toBinaryString(i) + " >>> 1) = " + j +
+                            " (" + Integer.toBinaryString(j) + ")");
+                            
+        System.out.println("Integer Division : integer i (" + i + ") / 2 = " + (i / 2));
+        System.out.println("Double Division : double k = i (" + i + ") / 2.0 = " + k);
+
+
     }
 
 }
