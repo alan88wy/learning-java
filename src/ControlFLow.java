dfd40a292594812a9cec0a0ab12ad8201ea1158f
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-07-03 19:33:02 +0800

    Added more examples

--- src/ControlFLow.java
+++ src/ControlFLow.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class ControlFLow {
     public static void main(String[] args) {
          // Final variable. Can be use in case statement in switch statement
@@ -36,7 +37,83 @@ public class ControlFLow {
                 break;
         }
 
+        // Ternary operator
 
+        String result = (v >= 12) ? ((v == 12) ? "= 12" :  "> 12"): "<= 12";
+
+        System.out.println(result);
+
+        System.out.println("");
+
+        int[] array = {0,1,2,3,4,5,6,7,8,9, 10, 11};
+
+        System.out.print("Printing array : ");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("");
+
+        // Using another method
+
+        System.out.print("Printing array using for each: ");
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("");
+
+        // swapping element
+        for (int i=0, j=array.length-1; i < j; i++, j--) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        System.out.print("Reversed array : ");
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("");
+
+        // Can do this too
+
+        System.out.print("Reverse array again : ");
+
+        for (int i=0, j=array.length-1, middle = array.length/2; i < middle; i++, j--) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("");
+
+        // In order for the following to work, we need to declare using Integer[] instead of int[]
+        //
+
+        Integer[] array2 = {0,1,2,3,4,5,6,7,8,9};
+
+        
+        System.out.println("\nReverse array using Collections.reverse : \n");
+
+        Collections.reverse(Arrays.asList(array2));
+        System.out.println("Reversed Array: " + Arrays.asList(array2));
+
+        System.out.print("Printing array : ");
+
+        for (int i : array2) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("");
 
     }
 }
