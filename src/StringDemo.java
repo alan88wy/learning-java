286c747cf1fb9cc694a225a298c4309a28601cf8
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-08-09 17:20:16 +0800

    Added more examples

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -3,8 +3,8 @@ public class StringDemo {
     public static void main(String[] args) {
         String s = "Hello World!";
 
-        System.out.println(s);
-        System.out.println("String Length : " + s.length());
+        System.out.println("s = " + s);
+        System.out.println("s.length() = " + s.length());
         System.out.println("s.equals(\"HELLO WORLD!\") = " + s.equals("HELLO WORLD!"));
         System.out.println("s.equalsIgnoreCase(\"HELLO WORLD!\") = " + s.equalsIgnoreCase("HELLO WORLD!"));
         System.out.println("s.compareTo(\"HELLO WORLD!\") = " + s.compareTo("HELLO WORLD!"));
