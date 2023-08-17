73aec8803e9faf3e2be5bf125f67881bd16e2a66
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-08-17 20:03:39 +0800

    added comment

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -1,3 +1,4 @@
+
 public class StringDemo {
 
     static void stringPool() {
@@ -6,16 +7,36 @@ public class StringDemo {
             String Pool
             ***********
 
-            
-        
+            The JVM can optimize the amount of memory allocated for them by storing only one copy of each literal
+            String in the pool. This process is called interning.
+
+            When we create a String variable and assign a value to it, the JVM searches the pool for a String of
+            equal value.
+
+            If found, the Java compiler will simply return a reference to its memory address, without allocating
+            additional memory.
+
+            When we create a String via the new operator, the Java compiler will create a new object and store it
+            in the heap space reserved for the JVM.
+
+            Every String created like this will point to a different memory region with its own address.
+
+            We can manually intern a String in the Java String Pool by calling the intern() method on the object
+            we want to intern.
+
+            Manually interning the String will store its reference in the pool, and the JVM will return this
+            reference when needed.
+
          */
 
 
         System.out.println("\nInside stringPool ...\n");
         String s1 = "hello!";
-        String s2 = "hello!";
-        String s3 = "hello!".intern();
-        String s4 = new String("hello!");	 
+        String s2 = "hello!";               // Will point to the same address of s1
+        String s3 = "hello!".intern();      // Will point to the same address of S1
+        String s4 = new String("hello!");   // This will point to a new string object in the heap which will 
+                                            // point to the same address of s1 but it is not the same as s1 unless you
+                                            // refer to the value using the intern() method
         String s5 = "lo!";
 
         System.out.println("String s1 = \"hello!\";");
@@ -30,6 +51,9 @@ public class StringDemo {
         System.out.println("s1 == s4.intern(): " + (s1 == s4.intern()));
         System.out.println("s1 == \"hel\" + \"lo!\": " + (s1 == "hel" + "lo!"));
         System.out.println("s1 == \"hel\" + s5: " + (s1 == "hel" + s5));
+
+       
+
     }
 
     public static void main(String[] args) {
@@ -46,7 +70,7 @@ public class StringDemo {
         System.out.println("\"HELLO WORLD!\".toLowerCase() To Lower Case =  \"" + "HELLO WORLD!".toLowerCase() + "\"");
         System.out.println("\"Hello World!\".toUpperCase() To Upper Case =  \"" + "Hello World!".toUpperCase() + "\"");
         System.out.println("s.join(\"-\", \"HELLO\", \"WORLD\",\"!\") = " + s.join("-", "HELLO", "WORLD", "!"));
-        
+
         String t[] = "I See a Fox Jumping Over The Fence !".split(" ");
 
         System.out.println("\"I See a Fox Jumping Over The Fence !\".split() : ");
@@ -57,5 +81,19 @@ public class StringDemo {
         System.out.println("");
 
         stringPool();
+
+         // StringBuffer()
+        StringBuffer sb1 = new StringBuffer("Hello World!");
+        sb1 = sb1.append(" Good Morning!");
+
+        System.out.println(sb1);
+        sb1.insert(0, "Ha ha ha ! ");
+        System.out.println(sb1);
+        sb1.replace(0, 11, "");
+        System.out.println(sb1);
+        sb1.delete(0, 13);
+        System.out.println(sb1);
+        sb1.reverse();
+        System.out.println(sb1);
     }
 }
