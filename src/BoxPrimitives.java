93deb03bd6afb2da61e2aad3f90cfa92aa895d20
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-09-22 21:54:53 +0800

    Added more example

--- src/BoxPrimitives.java
+++ src/BoxPrimitives.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BoxPrimitives {
 
     public static void main(String[] args) {
@@ -14,6 +16,38 @@ public class BoxPrimitives {
         System.out.println("in.compareTo(2) : " + (in.compareTo(2) == 0 ? true : false));
 
         System.out.println("in == 2 : " + (in == 2));
+
+        // Parsing Strings
+        int s2i = Integer.valueOf("20");
+        System.out.println("s2i (\"20\") : " + s2i);
+
+        String data = "4004	Effective Java Programming Language Guide	2007	T	4.9";
+        String[] items = data.split("\t");
+
+        long id = Long.parseLong(items[0]);
+        String title = items[1];
+        // int pubYear = Integer.parseInt(items[2]);
+        Integer pubYear = Integer.valueOf(items[2]);
+        char genre = items[3].charAt(0);
+        double rating = Double.parseDouble(items[4]);
+
+        System.out.println("id : " + id);
+        System.out.println("Title : " + title);
+        System.out.println("Publication Year : " + pubYear);
+        System.out.println("Genre : " + genre);
+        System.out.println("Rating : " + rating);
+
+        // Utility Methods
+        System.out.println("Genre is Letter ? " + Character.isLetterOrDigit(genre));
+        System.out.println("Genre is Digit ? " + Character.isDigit(genre));
+        System.out.println("Genre is Letter or Digit ? " + Character.isLetterOrDigit( genre)); 
+        System.out.println("Genre is UpperCase ? " + Character.isUpperCase(genre));
+        System.out.println("0.0 / 0.0 is NaN : " + Double.isNaN(0.0/0.0)); 
+        System.out.println("To Binary (18) : " + Integer.toBinaryString(18));
+        System.out.println("To Hex (18) : " + Integer.toHexString(18));
         
+        ArrayList aList = new ArrayList();
+        aList.add(Long.valueOf(20));
+        System.out.println(aList);
     }
 }
